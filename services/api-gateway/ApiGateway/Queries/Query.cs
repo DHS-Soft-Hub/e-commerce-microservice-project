8e5568acdef706ab34c9429b0d13a67e78a79dd1
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-09-02 08:50:32 +0200

    Update

--- services/api-gateway/ApiGateway/Queries/Query.cs
+++ services/api-gateway/ApiGateway/Queries/Query.cs
@@ -1,8 +1,12 @@
+using HotChocolate;
+using HotChocolate.Types;
+
 namespace ApiGateway.Queries
 {
     public class Query
     {
-        public Order GetOrder(string orderId)
+        [GraphQLName("order")]
+        public Order GetOrder([ID][GraphQLNonNullType] string orderId)
         {
             var orderQuery = new OrderQuery();
             return orderQuery.GetOrder(orderId);
