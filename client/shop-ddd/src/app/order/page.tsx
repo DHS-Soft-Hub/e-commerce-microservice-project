4ad5b56bf7b758a641c8a17c6bea7f89bf6b8e8e
hganchev <hristo.iliev.ganchev@gmail.com>
2025-09-03 14:31:34 +0300

    feat: Refactor OrderPage and OrdersRootProvider to use OrderRepository, removing GraphQL dependencies

--- client/shop-ddd/src/app/order/page.tsx
+++ client/shop-ddd/src/app/order/page.tsx
@@ -1,21 +1,18 @@
 'use client';
 
-import { OrderService } from "@/features/orders/application/services/OrderService";
-import { OrderRepository } from "@/features/orders/infrastructure/repositories/OrderRepository";
 import { OrderDialog } from "@/features/orders/presentations/components/OrderDialog";
-import { OrderServiceProvider } from "@/features/orders/presentations/providers/OrderServiceProvider";
-import { OrderApi } from '@/features/orders/infrastructure/api/OrderApi';
+import { OrdersRootProvider } from "@/features/orders/presentations/providers/OrdersRootProvider";
 
 export default function OrderPage() {
   return (
     <div>
       <h1>Order Page</h1>
-      <OrderServiceProvider service={new OrderService(new OrderRepository(new OrderApi()))}>
+      <OrdersRootProvider>
         <OrderDialog
           open={true}
           orderId={"123"}
           onClose={() => {}} />
-      </OrderServiceProvider>
+      </OrdersRootProvider>
     </div>
   );
 }
--- client/shop-ddd/src/features/orders/presentations/providers/OrdersRootProvider.tsx
+++ client/shop-ddd/src/features/orders/presentations/providers/OrdersRootProvider.tsx
@@ -3,13 +3,13 @@
 import React, { useMemo } from "react";
 import { OrderServiceProvider } from "./OrderServiceProvider";
 import { OrderApi } from "../../infrastructure/api/OrderApi";
-import { GraphQLOrderRepository } from "../../infrastructure/repositories/graphql/GraphQLOrderRespository";
+import { OrderRepository } from "../../infrastructure/repositories/OrderRepository";
 import { OrderService } from "../../application/services/OrderService";
 
 export const OrdersRootProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const service = useMemo(() => {
         const api = new OrderApi();
-        const repo = new GraphQLOrderRepository(api);
+        const repo = new OrderRepository(api);
         return new OrderService(repo);
     }, []);
 
